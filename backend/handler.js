1d04a8b01823f699d9a464b6e90ed45677ddced7
James Wade <jpswade@gmail.com>
2018-12-03 20:18:06 +0000

    Basic graphql query

--- backend/handler.js
+++ backend/handler.js
@@ -1,14 +1,41 @@
-'use strict';
+const {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull
+} = import("graphql");
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+// This method just inserts the user's first name into the greeting message.
+const getGreeting = firstName => `Hello, ${firstName}.`;
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+// Here we declare the schema and resolvers for the query
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "RootQueryType", // an arbitrary name
+    fields: {
+      // the query has a field called 'greeting'
+      greeting: {
+        // we need to know the user's name to greet them
+        args: {
+          firstName: {
+            name: "firstName",
+            type: new GraphQLNonNull(GraphQLString)
+          }
+        },
+        // the greeting message is a string
+        type: GraphQLString,
+        // resolve to a greeting message
+        resolve: (parent, args) => getGreeting(args.firstName)
+      }
+    }
+  })
+});
+
+// We want to make a GET request with ?query=<graphql query>
+// The event properties are specific to AWS. Other providers will differ.
+module.exports.query = (event, context, callback) =>
+  graphql(schema, event.queryStringParameters.query).then(
+    result => callback(null, { statusCode: 200, body: JSON.stringify(result) }),
+    err => callback(err)
+  );
