d1ed4b15aa8e38a76bfb5bc04299e26045160125
James Wade <jpswade@gmail.com>
2018-12-29 09:20:12 +0000

    rm unused code

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,3 @@
-const API_URL = process.env.API_URL ? process.env.API_URL : 'http://localhost:3000/';
-
-if (!API_URL) {
-  console.error('Set `API_URL` in `client/src/app/js/actions/index.js` to your stage endpoint')
-}
+const API_URL = process.env.API_URL ? process.env.API_URL : 'http://localhost:3000';
 
 export const QUERY_URL = `${API_URL}/query/`
