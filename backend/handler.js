5112e16600fff0c00689b7cbc0dd0eb0ca113454
James Wade <jpswade@gmail.com>
2018-12-30 15:20:57 +0000

    rm error handling

--- backend/handler.js
+++ backend/handler.js
@@ -4,8 +4,5 @@ const graphql = require ('./graphql');
 
 module.exports.graphql = (event, context, callback) => {
   graphql(event.body)
-  .then(
-      response => callback(null, { statusCode: 200, body: JSON.stringify(response) }),
-      error => callback(error)
-    );
+  .then(response => callback(null, { statusCode: 200, body: JSON.stringify(response) }))
 };
